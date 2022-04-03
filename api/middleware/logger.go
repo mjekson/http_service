83f054449c127e85f71085e95011bb49ec093497
mjekson <mjeksonmjekson@gmail.com>
2022-04-04 01:05:23 +0300

    add api

---
+++ api/middleware/logger.go
@@ -0,0 +1,12 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+)
+
+func RequestLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("Request: %s Method: %s", r.RequestURI, r.Method)
+	})
+}
---
+++ api/routes.go
@@ -0,0 +1,21 @@
+package api
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/mjekson/http_service/internals/app/handlers"
+)
+
+func CreateRoutes(userHandler *handlers.UsersHandler, carsHandler *handlers.CarsHandler) *mux.Router {
+	r := mux.NewRouter()
+	r.HandleFunc("/users/create", userHandler.Create).Methods("POST")
+	r.HandleFunc("/users/list", userHandler.List).Methods("GET")
+	r.HandleFunc("/users/find/{id:[0-9]+}", userHandler.Find).Methods("GET")
+
+	r.HandleFunc("/cars/create", carsHandler.Create).Methods("POST")
+	r.HandleFunc("/cars/list", carsHandler.List).Methods("GET")
+	r.HandleFunc("/cars/find/{id:[0-9]+}", carsHandler.Find).Methods("GET")
+
+	r.NotFoundHandler = r.NewRoute().HandlerFunc(handlers.NotFound).GetHandler()
+
+	return r
+}
